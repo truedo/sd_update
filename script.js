3c1babf856e1eb93f6f6fc1b61cc0240b346fad9
truedo <truedo@naver.com>
2025-02-26 13:20:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let reader;
 const BAUD_RATE = 921600;
 const TIMEOUT = 3000; // ms
 
-const VERSION_JS = '1.0.23'; 
+const VERSION_JS = '1.0.24'; 
 
 let BUFFER_SIZE = 64; // 버퍼 크기 설정
 let SEND_TERM = 50; // 명령간의 텀
@@ -154,7 +154,7 @@ class SDCardUploader
     }
   }
 }
-
+const uploader = new SDCardUploader();
 // // 사용 예시
 // const uploader = new SDCardUploader();
 // document.querySelector('#uploadBtn').addEventListener('click', async () => {
