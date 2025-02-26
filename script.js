2c375866b1d6236b81fa1b82b1a7b9fed5cb5f0f
truedo <truedo@naver.com>
2025-02-26 14:10:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let reader;
 const BAUD_RATE = 921600;
 const TIMEOUT = 3000; // ms
 
-const VERSION_JS = '1.0.28'; 
+const VERSION_JS = '1.0.29'; 
 
 let BUFFER_SIZE = 64; // 버퍼 크기 설정
 let SEND_TERM = 50; // 명령간의 텀
