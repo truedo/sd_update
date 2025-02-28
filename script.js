37a7cd1e7ecf1e174d07b5359ff404dd05a4722f
truedo <truedo@naver.com>
2025-02-28 12:33:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let reader;
 const BAUD_RATE = 921600;
 const TIMEOUT = 3000; // ms
 
-const VERSION_JS = '1.0.80'; 
+const VERSION_JS = '1.0.81'; 
 
 let BUFFER_SIZE = 64; // 버퍼 크기 설정
 let SEND_TERM = 50; // 명령간의 텀
@@ -80,8 +80,8 @@ class SDCardUploader
         ]);
         const receivedByte = value[0];
         if(receivedByte === 0xE1) return true;
-        if(receivedByte === 0xE2) throw new Error('CRC 오류');
-        if(receivedByte === 0xE3) throw new Error('크기 불일치');
+        if(receivedByte === 0xE2) throw new Error('크기 불일치');
+        if(receivedByte === 0xE3) throw new Error('파일 없음');
       } 
       catch(error) 
       {
