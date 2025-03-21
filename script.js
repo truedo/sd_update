8e6ef3cb799ef20a7ee0661bd9e76e781dd4538f
truedo <truedo@naver.com>
2025-03-21 17:34:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let reader;
 const BAUD_RATE = 921600;
 const TIMEOUT = 3000; // ms
 
-const VERSION_JS = '1.1.18'; 
+const VERSION_JS = '1.1.19'; 
 
 let BUFFER_SIZE = 64; // 버퍼 크기 설정
 let SEND_TERM = 50; // 명령간의 텀
@@ -191,16 +191,16 @@ class SDCardUploader
     console.log(`경로 길이 :${pathData.byteLength}`);
     await this.writer.write(this.packUint32LE(pathData.byteLength));
     await this.waitForACK();
-    //await new Promise(resolve => setTimeout(resolve, SEND_TERM));
-await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise(resolve => setTimeout(resolve, SEND_TERM));
+//await new Promise(resolve => setTimeout(resolve, 3000));
 
    // console.warn("경로 데이터 전송");
 
    
     // 🔶 2. 경로 데이터 전송
     await this.sendChunked(pathData);
-    //await new Promise(resolve => setTimeout(resolve, SEND_TERM));
-await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise(resolve => setTimeout(resolve, SEND_TERM));
+//await new Promise(resolve => setTimeout(resolve, 3000));
 
    // console.warn("파일 크기 전송");
 
@@ -208,7 +208,7 @@ await new Promise(resolve => setTimeout(resolve, 3000));
    // console.log(`📥 파일 크기: ${fileSize} bytes`);
     await this.writer.write(this.packUint32LE(fileSize));
   //  await this.writer.write(new Uint8Array(new Uint32Array([fileSize]).buffer));
-  console.log(`⌚검증 기다리기1`);
+  //console.log(`⌚검증 기다리기1`);
   await this.waitForACK();
   //  await new Promise(resolve => setTimeout(resolve, SEND_TERM));
   }
